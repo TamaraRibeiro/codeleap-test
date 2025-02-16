206d87b48cac4f5973cb1e572b0d77516a054f1a
TamaraRibeiro <tamara.fl@hotmail.com>
2025-02-16 00:32:52 -0300

    :tada: first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+
+  return (
+    <>
+      <div className="">
+       <p className="text-2xl text-amethyst-950">hello codeleap test</p>
+      </div>
+      
+    </>
+  )
+}
